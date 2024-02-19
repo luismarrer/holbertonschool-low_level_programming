61d098964c748c8596c058456e7b3b6fec2b32df
Luis <lmarrero1070@gmail.com>
2024-02-19 19:27:17 -0400

    101-

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <unistd.h>
+/**
+ * main - main is void
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
