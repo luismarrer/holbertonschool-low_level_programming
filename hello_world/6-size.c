e156e4789d5e554e2585ce92693eb443be8c4297
Luis <lmarrero1070@gmail.com>
2024-02-19 19:19:50 -0400

    fix 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+* main - print a size with sizeof
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
+
+	return (0);
+}
