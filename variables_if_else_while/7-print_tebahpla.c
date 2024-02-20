cc295c027e75ad2debde9e03868774690359653d
Luis <lmarrero1070@gmail.com>
2024-02-20 03:19:09 -0400

    adding 7-

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
