effe68a708636709f552d886715ef0febb657deb
Luis <lmarrero1070@gmail.com>
2024-02-21 21:44:55 -0400

    update

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - this is the intenger function
+ * @n: checking this one
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
