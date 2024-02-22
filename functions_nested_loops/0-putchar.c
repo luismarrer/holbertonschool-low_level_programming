2afa7f40aa5236d8bd62f1b673cfa4026395f57c
Luis <lmarrero1070@gmail.com>
2024-02-21 21:25:39 -0400

    update

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,14 +9,18 @@
 
 int main(void)
 {
-	char *str = "_putchar";
 
-	while (*str)
-	{
-		_putchar(*str++);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
+
 }
 
