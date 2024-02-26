a935980e13b7f3a0a98590f8564c424062328963
Luis <lmarrero1070@gmail.com>
2024-02-26 18:30:02 -0400

    adding 3-puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed by a new line.
+ *
+ * @str: The string
+ *
+ */
+
+void _puts(char *str)
+
+{
+
+	while (*str)
+
+	{
+		 _putchar(*str++);
+	}
+
+	_putchar('\n');
+
+}
+
+
