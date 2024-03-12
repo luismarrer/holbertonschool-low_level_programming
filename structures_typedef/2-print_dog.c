798631b71624e2732e22355725909956f036edf7
Luis <lmarrero1070@gmail.com>
2024-03-11 21:41:19 -0400

    fix 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -24,7 +24,7 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: %s\n", d->name);
 		}
-			printf("Age: %.1f\n", d->age);
+			printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
