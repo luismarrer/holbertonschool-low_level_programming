ce27a983e280b2fa8810b48a692a4ee873da1ed6
Luis <lmarrero1070@gmail.com>
2024-03-19 02:50:59 -0400

    fix .h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,23 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct format - choose the format to print.
+ *
+ * @format: types of arguments.
+ *
+ * @function: The function associated to the type argument.
+ *
+ */
+
+typedef struct format
+{
+char *format;
+void (*function)(char *separator, va_list arg);
+} format_t;
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
